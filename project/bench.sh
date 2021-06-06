35a1da479bd6f71afe6c42523d3e59d51059ebc0
GiovanniZotta <giovannizotta771@gmail.com>
2021-06-06 23:28:16 +0200

    Added script to generate qsub files for benchmarking

--- project/bench.sh
+++ project/bench.sh
@@ -5,18 +5,49 @@ BIN="hpc4ds/project/bin/main.out"
 N_NODES=4
 N_CPU=4
 RAM=32
-FILENAME="data/freq-items-tsz/0.5_tsz100_tct5.0m.txt" 
-N_PROC=4
-N_THREAD=4
-MIN_SUPPORT=50
 DEBUG=1
+rm -rf sub_scripts
+mkdir -p sub_scripts
 
-cat > test.txt <<EOF
-#PBS -l select=${N_NODES}:ncpus=${N_CPU}:mem=${RAM}gb:net_type=IB
+
+for MIN_SUPPORT in 0.05 0.1 0.2
+do
+    for N_PROC in 1 2 4
+    do
+        for N_THREAD in 1 2 4
+        do
+            for D in data/*
+            do
+                for FILE in ${D}*
+                do
+                    if [ -f "${FILE}" ]
+                    then
+                        FILENAME=$(basename ${FILE})
+                        TIMESTAMP=$(date +"%Y-%m-%d_%H-%M-%S")
+                        echo ${FILENAME}
+                        echo ${TIMESTAMP}
+                        sleep 1
+cat > sub_scripts/${TIMESTAMP}_${MIN_SUPPORT}_${N_PROC}_${N_THREAD}_${FILENAME}_sub.sh <<EOF
+#PBS -l select=${N_PROC}:ncpus=${N_THREAD}:mem=${RAM}gb:net_type=IB
 
 #PBS -l walltime=0:50:30
 
 #PBS -q short_cpuQ
 
-network $HPC
+#PBS -o out_${TIMESTAMP}_${MIN_SUPPORT}_${N_PROC}_${N_THREAD}_${FILENAME}
+
+#PBS -e err_${TIMESTAMP}_${MIN_SUPPORT}_${N_PROC}_${N_THREAD}_${FILENAME}
+
+
+module load mpich-3.2
+
+/usr/bin/time -v mpirun.actual -n ${N_PROC} ${BIN} ${FILE} ${N_THREAD} ${MIN_SUPPORT} ${DEBUG} 
 EOF
+                        chmod +x sub_scripts/${TIMESTAMP}_${MIN_SUPPORT}_${N_PROC}_${N_THREAD}_${FILENAME}_sub.sh
+                    fi
+                done
+            done
+        done
+    done
+done
+
