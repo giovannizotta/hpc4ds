cfce97ce6ff40be2a20199c136bc62249b6cc05a
GiovanniZotta <giovannizotta771@gmail.com>
2021-06-08 20:39:53 +0200

    Updated benchmark script

--- project/bench.sh
+++ project/bench.sh
@@ -1,6 +1,7 @@
  #!/bin/bash
 
 BIN="hpc4ds/project/bin/main.out"
+DATA_DIR=$1
 
 N_NODES=4
 N_CPU=4
@@ -8,43 +9,55 @@ RAM=32
 DEBUG=1
 rm -rf sub_scripts
 mkdir -p sub_scripts
+rm -rf sub_results
+mkdir -p sub_results
 
-
-for MIN_SUPPORT in 0.05 0.1 0.2
+echo $DATA_DIR
+for D in ${DATA_DIR}/*
 do
-    for N_PROC in 1 2 4
+    mkdir -p sub_results/$(basename ${D})
+    # echo $(basename ${D})
+done
+
+for ITER in 1 2 3 4 5 
+do
+    for MIN_SUPPORT in 0.05 0.1 0.2
     do
-        for N_THREAD in 1 2 4
+        for N_PROC in 1 2 4
         do
-            for D in data/*
+            for N_THREAD in 1 2 4
             do
-                for FILE in ${D}*
+                for D in ${DATA_DIR}/*
                 do
-                    if [ -f "${FILE}" ]
-                    then
-                        FILENAME=$(basename ${FILE})
-                        TIMESTAMP=$(date +"%Y-%m-%d_%H-%M-%S")
-                        echo ${FILENAME}
-                        echo ${TIMESTAMP}
-                        sleep 1
-cat > sub_scripts/${TIMESTAMP}_${MIN_SUPPORT}_${N_PROC}_${N_THREAD}_${FILENAME}_sub.sh <<EOF
+                    for FILE in ${D}/*
+                    do
+                        if [ -f "${FILE}" ]
+                        then
+                            FILENAME=$(basename ${FILE})
+                            TIMESTAMP=$(date +"%Y-%m-%d-%H-%M-%S")
+                            SUB_NAME=${TIMESTAMP}_${ITER}_${MIN_SUPPORT}_${N_PROC}_${N_THREAD}_${FILENAME}
+                            echo ${SUB_NAME}
+                            sleep 1
+cat > sub_scripts/sub.sh <<EOF
 #PBS -l select=${N_PROC}:ncpus=${N_THREAD}:mem=${RAM}gb:net_type=IB
 
 #PBS -l walltime=0:50:30
 
 #PBS -q short_cpuQ
 
-#PBS -o out_${TIMESTAMP}_${MIN_SUPPORT}_${N_PROC}_${N_THREAD}_${FILENAME}
+#PBS -o sub_results/$(basename ${D})/out_${SUB_NAME}
 
-#PBS -e err_${TIMESTAMP}_${MIN_SUPPORT}_${N_PROC}_${N_THREAD}_${FILENAME}
+#PBS -e sub_results/$(basename ${D})/err_${SUB_NAME}
 
 
 module load mpich-3.2
 
 /usr/bin/time -v mpirun.actual -n ${N_PROC} ${BIN} ${FILE} ${N_THREAD} ${MIN_SUPPORT} ${DEBUG} 
 EOF
-                        chmod +x sub_scripts/${TIMESTAMP}_${MIN_SUPPORT}_${N_PROC}_${N_THREAD}_${FILENAME}_sub.sh
-                    fi
+                            chmod +x sub_scripts/sub.sh
+                            qsub sub_scripts/sub.sh
+                        fi
+                    done
                 done
             done
         done
